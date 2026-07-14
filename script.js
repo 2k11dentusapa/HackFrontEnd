10fc062e9c363c335973b3c798ad24ee11f0d3a7
2k11dentusapa <nguyentruongthaogiang@gmail.com>
2026-07-14 09:52:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (() => {
     const TARGET_WIDTH = 1280;
-    const TARGET_HEIGHT = 720;
+    const TARGET_HEIGHT = 593;
     const TOLERANCE = 100;
     setInterval(() => {
         if (Math.abs(window.innerWidth - TARGET_WIDTH) > TOLERANCE || Math.abs(window.innerHeight - TARGET_HEIGHT) > TOLERANCE) {
